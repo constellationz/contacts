0af369b3d7f0c188b4965e065b3a2e59598294d5
SopaKitchen <122580569+SopaKitchen@users.noreply.github.com>
2023-01-22 13:14:19 -0500

    Update Login.php

--- LAMPAPI/Login.php
+++ LAMPAPI/Login.php
@@ -15,13 +15,13 @@
 	else
 	{
 		$stmt = $conn->prepare("SELECT ID,FirstName,LastName FROM Users WHERE Login=? AND Password =?");
-		$stmt->bind_param("ss", $inData["Login"], $inData["Password"]);
+		$stmt->bind_param("ss", $inData["login"], $inData["password"]);
 		$stmt->execute();
 		$result = $stmt->get_result();
 
 		if( $row = $result->fetch_assoc()  )
 		{
-			returnWithInfo( $row['FirstName'], $row['LastName'], $row['ID'] );
+			returnWithInfo( $row['firstName'], $row['lastName'], $row['ID'] );
 		}
 		else
 		{
