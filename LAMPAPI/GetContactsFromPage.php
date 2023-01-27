25aebbff17a673524aed2954dd2f47c6db4830d5
SopaKitchen <122580569+SopaKitchen@users.noreply.github.com>
2023-01-26 20:24:29 -0500

    Add files via upload

---
+++ LAMPAPI/GetContactsFromPage.php
@@ -0,0 +1,67 @@
+<?php
+
+	$inData = getRequestInfo();
+	
+	$searchResults = "";
+	$searchCount = 0;
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+		$stmt = $conn->prepare("select ID, Name, Phone, Email from Contacts where UserID=? limit ? offset ?");
+		$stmt->bind_param("i", $inData["userId"]);
+		$stmt->execute();
+		
+		$result = $stmt->get_result();
+		
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '{"id":'.$row["ID"].',"name":"'.$row["Name"].'","phone":"'.$row["Phone"].'","email":"'.$row["Email"].'"}';
+		}
+		
+		if( $searchCount == 0 )
+		{
+			returnWithError( "User Has No Records" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+		
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+	function returnWithInfo( $searchResults )
+	{
+		$retValue = '{"results":[' . $searchResults . '],"error":""}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?>
